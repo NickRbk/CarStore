33ec7c116025e3caf17f5d95f16dc87119a85cbc
Nick Rybak <nrybak94@gmail.com>
2018-05-25 23:11:08 +0300

    dev: add Seller @Entity

---
+++ src/main/java/cursor/rybak/store/model/Seller.java
@@ -0,0 +1,21 @@
+package cursor.rybak.store.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "sellers")
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String first_name;
+    private String last_name;
+    private LocalDate time_on_app;
+    private String mobile_phone;
+}
