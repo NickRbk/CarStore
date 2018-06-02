515903bdece2fc77a4414f389c1c81a60a70f09c
Nick Rybak <nrybak94@gmail.com>
2018-06-02 21:21:51 +0300

    experimental: add service layer for cars

--- src/main/java/cursor/rybak/store/controller/CarController.java
+++ src/main/java/cursor/rybak/store/controller/CarController.java
@@ -1,10 +1,8 @@
 package cursor.rybak.store.controller;
 
-import cursor.rybak.store.exception.NotFoundException;
 import cursor.rybak.store.model.Car;
-import cursor.rybak.store.repository.car.CarRepository;
-import cursor.rybak.store.repository.seller.SellerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import cursor.rybak.store.service.CarService;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -13,61 +11,37 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@AllArgsConstructor
 public class CarController {
 
-    private final CarRepository carRepository;
-    private final SellerRepository sellerRepository;
-
-    @Autowired
-    public CarController(CarRepository carRepository, SellerRepository sellerRepository) {
-        this.carRepository = carRepository;
-        this.sellerRepository = sellerRepository;
-    }
+    private CarService carService;
 
     @GetMapping("/sellers/{sellerId}/cars")
     public Page<Car> getAllCarsBySellerId(@PathVariable(value = "sellerId") Long sellerId,
                                           Pageable pageable) {
 
-        return carRepository.findBySellerId(sellerId, pageable);
+        return carService.getAllCarsBySellerId(sellerId, pageable);
     }
 
     @PostMapping("/sellers/{sellerId}/cars")
-    public Car createCar(@PathVariable(value = "sellerId") Long sellerId,
-                         @Valid @RequestBody Car car) {
+    public Car addCarBySellerId(@PathVariable(value = "sellerId") Long sellerId,
+                                @Valid @RequestBody Car car) {
 
-        return sellerRepository.findById(sellerId)
-                .map(seller -> {
-                    car.setSeller(seller);
-                    return carRepository.save(car);
-                }).orElseThrow(() -> new NotFoundException("SellerId " + sellerId + " not found!"));
+        return carService.add(sellerId, car);
     }
 
     @PutMapping("/sellers/{sellerId}/cars/{carId}")
-    public Car updateCar(@PathVariable(value = "sellerId") Long sellerId,
-                         @PathVariable(value = "carId") Long carId,
-                         @Valid @RequestBody Car carReq) {
+    public Car updateCarByCarId(@PathVariable(value = "sellerId") Long sellerId,
+                                @PathVariable(value = "carId") Long carId,
+                                @Valid @RequestBody Car carReq) {
 
-        if(!sellerRepository.existsById(sellerId)) {
-            throw new NotFoundException("SellerId " + carId + " not found!");
-        }
-
-        return carRepository.findById(carId).map(car -> {
-            car.setDescription(carReq.getDescription());
-            return carRepository.save(car);
-        }).orElseThrow(() -> new NotFoundException("CarId " + carId + "not found"));
+        return carService.update(sellerId, carId, carReq);
     }
 
     @DeleteMapping("/sellers/{sellerId}/cars/{carId}")
-    public ResponseEntity<?> deleteCar(@PathVariable(value = "sellerId") Long sellerId,
-                                       @PathVariable(value = "carId") Long carId) {
+    public ResponseEntity<?> deleteCarByCarId(@PathVariable(value = "sellerId") Long sellerId,
+                                              @PathVariable(value = "carId") Long carId) {
 
-        if(!sellerRepository.existsById(sellerId)) {
-            throw new NotFoundException("SellerId " + carId + " not found!");
-        }
-
-        return carRepository.findById(carId).map(car -> {
-            carRepository.delete(car);
-            return ResponseEntity.ok().build();
-        }).orElseThrow(() -> new NotFoundException("CarId " + carId + "not found"));
+        return carService.delete(sellerId, carId);
     }
 }
--- src/main/java/cursor/rybak/store/repository/car/CarRepository.java
+++ src/main/java/cursor/rybak/store/repository/car/CarRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
     Page<Car> findBySellerId(Long sellerId, Pageable pageable);
+
+    Page<Car> findAll(Pageable pageable);
 }
