ba7d9ef3591eef1c8a0d10218e92c704f19b2a32
Nick Rybak <nrybak94@gmail.com>
2018-06-18 23:59:30 +0300

    experimental: get all cars for anonymous user

--- src/main/java/cursor/rybak/store/web/controller/CarController.java
+++ src/main/java/cursor/rybak/store/web/controller/CarController.java
@@ -2,46 +2,29 @@ package cursor.rybak.store.web.controller;
 
 import cursor.rybak.store.domain.model.Car;
 import cursor.rybak.store.service.ICarService;
-import cursor.rybak.store.web.dto.CarDTO;
-import cursor.rybak.store.web.dto.SellerDTO;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Validated
 @RestController
+@RequestMapping("/store")
 @AllArgsConstructor
 public class CarController {
 
-//    private ICarService carService;
-//
-//    @GetMapping("/sellers/{sellerId}/cars")
-//    public Page<Car> getAllCarsBySellerId(@PathVariable(value = "sellerId") Long sellerId,
-//                                          Pageable pageable) {
-//
-//        return carService.getAllCarsBySellerId(sellerId, pageable);
-//    }
-//
-//    @PostMapping("/sellers/{sellerId}/cars")
-//    public Car addCarBySellerId(@PathVariable(value = "sellerId") Long sellerId,
-//                                @RequestBody
-//                                @NotNull
-//                                @Valid CarDTO carDTO) {
-//
-//        return carService.add(sellerId, carDTO);
-//    }
+    private ICarService carService;
 
-//    @PutMapping("/sellers/{sellerId}/cars/{carId}")
-//    public Car updateCarByCarId(@PathVariable(value = "sellerId") Long sellerId,
-//                                @PathVariable(value = "carId") Long carId,
-//                                @Valid @RequestBody Car carReq) {
-//
-//        return carService.update(sellerId, carId, carReq);
-//    }
+    @Transactional
+    @GetMapping()
+    public List<Car> getAllCars() {
+
+        return carService.getAllAsStream()
+                .collect(Collectors.toList());
+    }
 }
