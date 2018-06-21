12c73d295427a770c27865e18d13a8a628a3207a
Nick Rybak <nrybak94@gmail.com>
2018-06-21 21:41:11 +0300

    experimental: remove redundant code

--- src/main/java/cursor/rybak/store/domain/model/Car.java
+++ src/main/java/cursor/rybak/store/domain/model/Car.java
@@ -1,8 +1,6 @@
 package cursor.rybak.store.domain.model;
 
 import lombok.*;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -15,27 +13,18 @@ import javax.persistence.*;
 @ToString
 @Table(name = "cars")
 public class Car extends Audit {
+    public Double price;
+    public Integer year;
+    @Column(name = "country_of_registration")
+    public String countryOfRegistration;
+    public String type;
+    public String model;
+    public String mark;
+    public String description;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Double price;
-
-    public Integer year;
-
-    @Column(name = "country_of_registration")
-    public String countryOfRegistration;
-
-    public String type;
-
-    public String model;
-
-    public String mark;
-
-    public String description;
-
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "seller_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Seller seller;
 }
